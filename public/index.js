5762c0f431ea2a20ad127dd0305efab1b90267f8
Leon <leon.muriuki@student.moringaschool.com>
2023-11-01 19:36:53 +0300

    Corrected database

--- public/index.js
+++ public/index.js
@@ -1,22 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => { //This code runs once the DOM content is completely loaded and ready for modification.
-    // Define the URL of the JSON API
-const apiUrl = 'https://raw.githubusercontent.com/Mugo-leon/Phase1-Project-FindMyMentor/main/db.json';
+document.addEventListener("DOMContentLoaded", () => {
+  const baseUrl = "https://my-json-server.typicode.com/Mugo-leon/Phase1-Project-FindMyMentor";
+  console.log("API base URL:", baseUrl);
 
-// Make a GET request to the API
-fetch(apiUrl)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json(); 
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.error('There was a problem with the fetch operation:', error);
+  const mentorHeader = document.getElementById("mentor-header");
+  mentorHeader.style.display = "none";
+
+  const DisplayConsultantDetails = (consultantId) => {
+    fetch(`${baseUrl}/consultants/${consultantId}`)
+      .then((response) => response.json())
+      .then((consultantInfo) => {
+        const consultantChallenge = document.getElementById("consultant-challenge");
+        const consultantDescription = document.getElementById("consultant-description");
+        const consultantImage = document.getElementById("consultant-image");
+        const reviewList = document.getElementById("review-list");
+
+        consultantChallenge.textContent = consultantInfo.challenge;
+        consultantDescription.textContent = consultantInfo.description;
+        consultantImage.src = consultantInfo.image_url;
+
+        reviewList.innerHTML = "";
+        consultantInfo.reviews.forEach((review) => {
+          const clientrev = document.createElement("li");
+          clientrev.textContent = review;
+          reviewList.appendChild(clientrev);
+        });
+        mentorHeader.style.display = "block";
+      });
+  };
+
+  const DisplayAllConsultants = () => {
+    fetch(`${baseUrl}/consultants`)
+      .then((response) => response.json())
+      .then((consultants) => {
+        const challengeList = document.getElementById("challenge-list");
+        challengeList.innerHTML = "";
+
+        consultants.forEach((consultant) => {
+          const clientrev = document.createElement("li");
+          clientrev.textContent = consultant.challenge;
+          clientrev.addEventListener("click", () => {
+            DisplayConsultantDetails(consultant.id);
+          });
+          challengeList.appendChild(clientrev);
+        });
+
+        if (consultants.length === 0) {
+          const placeholder = document.createElement("li");
+          placeholder.textContent = "No consultants available.";
+          challengeList.appendChild(placeholder);
+          mentorHeader.style.display = "none";
+        }
+      });
+  };
+
+  const reviewForm = document.getElementById("review-form");
+  reviewForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const newReview = document.getElementById("review").value;
+    const reviewList = document.getElementById("review-list");
+    const clientrev = document.createElement("li");
+    clientrev.textContent = newReview;
+    reviewList.appendChild(clientrev);
+
+    document.getElementById("review").value = "";
   });
+  const consultantChallenge = document.getElementById("consultant-challenge");
+  const consultantDescription = document.getElementById("consultant-description");
+  const consultantImage = document.getElementById("consultant-image");
 
-
+  consultantChallenge.textContent = "Please select a challenge";
+  consultantDescription.textContent = "Select a challenge to view consultant details";
+  consultantImage.src = "images/placeholder.png";
   
+  DisplayAllConsultants();
 });
+
